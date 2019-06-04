679b1329e689e3e673fe11fb6d549a13b4495cc4
Aaron Chapman <aaron.chapman@gsa.gov>
2019-06-04 18:30:00 -0400

    added test coverage

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -22,4 +22,48 @@ describe SessionsController do
       end
     end
   end
+
+  describe 'POST auth/saml/setup' do
+    context 'when using IAL1 authentication' do
+      it 'sets assurance level to LOA1' do
+        request.env['omniauth.strategy'] = instance_double('Strategy', :options => { saml: [] })
+        post :setup, params: { provider: :saml, ial: '1' }
+
+        authn_context = request.env['omniauth.strategy'].options[:authn_context]
+        expected_result = [
+          "http://idmanagement.gov/ns/assurance/loa/1",
+          "http://idmanagement.gov/ns/requested_attributes?ReqAttr=email,phone,first_name,last_name,ssn"
+        ]
+        expect(authn_context).to eq(expected_result)
+      end
+    end
+
+    context 'when using IAL2 authentication' do
+      it 'sets assurance level to LOA3' do
+        request.env['omniauth.strategy'] = instance_double('Strategy', :options => { saml: [] })
+        post :setup, params: { provider: :saml, ial: '2' }
+
+        authn_context = request.env['omniauth.strategy'].options[:authn_context]
+        expected_result = [
+          "http://idmanagement.gov/ns/assurance/loa/3",
+          "http://idmanagement.gov/ns/requested_attributes?ReqAttr=email,phone,first_name,last_name,ssn"
+        ]
+        expect(authn_context).to eq(expected_result)
+      end
+    end
+
+    context 'when IAL has not been selected' do
+      it 'sets assurance level to LOA1' do
+        request.env['omniauth.strategy'] = instance_double('Strategy', :options => { saml: [] })
+        post :setup, params: { provider: :saml, ial: nil }
+
+        authn_context = request.env['omniauth.strategy'].options[:authn_context]
+        expected_result = [
+          "http://idmanagement.gov/ns/assurance/loa/1",
+          "http://idmanagement.gov/ns/requested_attributes?ReqAttr=email,phone,first_name,last_name,ssn"
+        ]
+        expect(authn_context).to eq(expected_result)
+      end
+    end
+  end
 end
